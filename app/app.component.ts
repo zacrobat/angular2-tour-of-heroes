3063123aee815286d1e0569a4dc9b144ed3612bb
Zac Cobb <zacrobat55@gmail.com>
2016-06-09 15:38:14 +1000

    Add click handler, expose selected hero, hide empty detail with nglf

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 export class Hero {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 @Component({
-  selector: 'my-app',
-  template:`
+    selector: 'my-app',
+    template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
     `
-    styles:[`
+    styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -71,22 +74,20 @@ export class Hero {
 
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  public heroes = HEROES;
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+    public heroes = HEROES;
 }
 var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
 ];
